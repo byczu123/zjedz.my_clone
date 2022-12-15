79068614093ddb34a22abf93bf2053e1ecf40a71
Dziomek <dziomek@student.agh.edu.pl>
2022-12-15 20:42:06 +0100

    registration

--- node_backend/controllers/user.js
+++ node_backend/controllers/user.js
@@ -1,15 +1,33 @@
+import { db } from "../db.js"
+
 export const register = (req, res) => {
     const email = req.body.email
     const password = req.body.password
     
     if (password && email) {
         console.log(email, password)
-        const data = {
-            email: email, 
-            password: password
-        }
-        const dataJSON = JSON.stringify(data)
-        res.send(dataJSON)
+        // const data = {
+        //     email: email, 
+        //     password: password
+        // }
+        // const dataJSON = JSON.stringify(data)
+        // res.send(dataJSON)
+
+        const sql = `INSERT INTO user (email, username, password_hash) VALUES ('${email}', 'user', '${password}');`
+        console.log(sql)
+        db.query(sql, (error, results) => {
+            if (error) {
+                res.send({
+                    error: true,
+                    message: 'Wystąpił błąd podczas rejestracji użytkownika.'
+                  });
+            } else {
+                res.send({
+                    error: false,
+                    message: 'Użytkownik został zarejestrowany pomyślnie.'
+                  });
+            }
+        })
     }
 }
 
