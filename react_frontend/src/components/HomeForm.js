fb1b4a7bee7aae768e1a71d125fff3cbe1231eca
Dziomek <dziomek@student.agh.edu.pl>
2023-01-04 17:52:57 +0100

    date, hour and number of people form

--- react_frontend/src/components/HomeForm.js
+++ react_frontend/src/components/HomeForm.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect, useContext, createContext } from 'react'
 import '../styles/HomeForm.css'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import { Dropdown } from 'react-bootstrap'
-import LocationContext from '../context/locationContext'
 import { Context } from '../context/appContext'
 
 
 const HomeForm = () => {
+  
   const [locations, setLocations] = useState(null)
-  const [days, setDays] = useState(null)
-  const [hours, setHours] = useState(null)
-  const [people, setPeople] = useState(null)
+  const [locationValue, setLocationValue] = useState('Wszystkie miasta')
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0])
+  const [hour, setHour] = useState("14:00")
+  const [peopleValue, setPeopleValue] = useState("2 osoby")
+  
   const {store, actions} = useContext(Context)
 
-  const [locationValue, setLocationValue] = useState('Wszystkie miasta')
-  const [dayValue, setDayValue] = useState('')
-  const [hourValue, setHourValue] = useState('')
-  const [peopleValue, setPeopleValue] = useState('')
+  const hours = ["14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"]
+  const people = ["2 osoby", "3 osoby", "4 osoby", "5 osób", "6 osób"]
 
-  console.log('HomeForm rendered. Location: ', store.currentLocation)
+  console.log('HomeForm rendered. Data: ', store.currentLocation, hour, date, peopleValue)
 
   useEffect(() => {
     fetch('/data/get/home-form')
@@ -42,23 +42,36 @@ const HomeForm = () => {
               </div>
               <div className="home-form-dropdown">
                 <DropdownButton id="dropdown-item-button" title={locationValue}>
-                  {
-                  locations ? locations.map((value, index) => {
+                {
+                  locations && locations.map((value, index) => {
                     return <Dropdown.Item key={index} as="button" onClick={() => {
                       console.log(value.location)
                       setLocationValue(value.location)
                       actions.setCurrentLocation(value.location)
                     }}>{value.location}</Dropdown.Item>
                   }) 
-                  : null
-                  }
+                }
                 </DropdownButton>
                 {/* <DropdownButton id="dropdown-item-button" type="date" title="2022-11-21">
                 </DropdownButton> */}
-                <input type="date" id="dropdown-item-button"></input>
-                <DropdownButton id="dropdown-item-button" title="18:00">
+                <input type="date" id="dropdown-item-button" value={date} onChange={(e) => {setDate(e.target.value)}}></input>
+                <DropdownButton id="dropdown-item-button" title={hour}>
+                {
+                  hours && hours.map((hour, index) => {
+                    return <Dropdown.Item key={index} as="button" onClick={() => {
+                      setHour(hour)
+                    }}>{hour}</Dropdown.Item>
+                  }) 
+                }
                 </DropdownButton>
-                <DropdownButton id="dropdown-item-button" title="2 osoby">
+                <DropdownButton id="dropdown-item-button" title={peopleValue}>
+                {
+                  people && people.map((value, index) => {
+                    return <Dropdown.Item key={index} as="button" onClick={() => {
+                      setPeopleValue(value)
+                    }}>{value}</Dropdown.Item>
+                  }) 
+                }
                 </DropdownButton>
               </div>
               <div className="home-form-description">
