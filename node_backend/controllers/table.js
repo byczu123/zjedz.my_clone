2d320da3f1d3bb222c5f061e53cad89942e9ea6f
byczu123 <byczek@student.agh.edu.pl>
2023-01-05 17:56:11 +0100

    display data in restaurant page

--- node_backend/controllers/table.js
+++ node_backend/controllers/table.js
@@ -3,7 +3,7 @@ import { db } from "../db.js"
 export const getTables = (req, res) => {
     const restaurant_id = req.params.restaurant_id
     console.log(restaurant_id)
-    const sql = `SELECT table_id, restaurant_id, table_position, price, number_of_places FROM restaurant_table WHERE restaurant_id='${restaurant_id}';`
+    const sql = `SELECT restaurant_id, number_of_places FROM restaurant_table WHERE restaurant_id='${restaurant_id}';`
     console.log(sql)
     db.query(sql, (error, results) => {
         if (error) {
--- node_backend/views/restaurant.js
+++ node_backend/views/restaurant.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getRestaurants } from "../controllers/restaurant.js";
+import { getRestaurants, getRestaurant } from "../controllers/restaurant.js";
 
 const router = express.Router()
 
 router.get('/get', getRestaurants)
+router.get('/get/:restaurant_id', getRestaurant)
 
 export default router
