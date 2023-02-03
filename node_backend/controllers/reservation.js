96dd518229896c435ade32ce0a3c9f073c9aec8b
Dziomek <dziomek@student.agh.edu.pl>
2023-02-03 10:49:26 +0100

    reservation confirm and check

--- node_backend/controllers/reservation.js
+++ node_backend/controllers/reservation.js
@@ -1,35 +1,53 @@
 import { db } from "../db.js"
 
 export const submitReservation = (req, res) => {
-    const userId = req.body.userId
-    const restaurantId = req.body.restaurantId
-    const date = req.body.date
-    const hour = req.body.hour
-    const price = req.body.price
-    const people = req.body.peopleValue
-
-    const sql = `UPDATE reservation
-    SET user_id='${userId}'
-    WHERE restaurant_id='${restaurantId}' AND date='${date}' AND hour='${hour}' AND price='${price}' AND people='${people}'`;
-    // console.log(sql)
-    db.query(sql, (error, results) => {
-        if (error) {
-            console.log(error)
-            res.send({
-                error: true,
-                message: 'Wystąpił błąd podczas rezerwacji.'
-                
-            });
+    const userId = req.body.userId;
+    const restaurantId = req.body.restaurantId;
+    const date = req.body.date;
+    const hour = req.body.hour;
+    const price = req.body.price;
+    const people = req.body.peopleValue;
+  
+    const checkSql = `SELECT * FROM reservation WHERE restaurant_id='${restaurantId}' AND date='${date}' AND hour='${hour}' AND price='${price}' AND people='${people}' AND user_id IS NOT NULL`;
+  
+    db.query(checkSql, (error, results) => {
+      if (error) {
+        console.log(error);
+        res.send({
+          error: true,
+          message: "Wystąpił błąd podczas sprawdzania rezerwacji."
+        });
+      } else {
+        if (results.length > 0) {
+          res.send({
+            error: true,
+            message: "Rezerwacja nie jest już dostepna."
+          });
         } else {
-            console.log(results)
-            res.send({
+          const updateSql = `UPDATE reservation
+          SET user_id='${userId}'
+          WHERE restaurant_id='${restaurantId}' AND date='${date}' AND hour='${hour}' AND price='${price}' AND people='${people}'`;
+  
+          db.query(updateSql, (error, results) => {
+            if (error) {
+              console.log(error);
+              res.send({
+                error: true,
+                message: "Wystąpił błąd podczas rezerwacji."
+              });
+            } else {
+              console.log(results);
+              res.send({
                 error: false,
-                message: 'Rezerwacja zatwierdzona.',
+                message: "Rezerwacja zatwierdzona.",
                 registered: true
-            });
-        }   
-    })
-}
+              });
+            }
+          });
+        }
+      }
+    });
+  };
 
 export const getFirstHours = (req, res) => {
     const restaurantId = req.body.restaurantId
--- react_frontend/src/components/ReservationSummary.js
+++ react_frontend/src/components/ReservationSummary.js
@@ -2,14 +2,15 @@ import React, { useContext, useState } from 'react'
 import '../styles/ReservationSummary.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faClock } from "@fortawesome/free-regular-svg-icons";
-import { faClock, faCalendar, faHouse, faUsers, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faCalendar, faHouse, faUsers, faUser, faXmark, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { Context } from '../context/appContext';
-import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 
 const ReservationSummary = (props) => {
 
     const {store, actions} = useContext(Context)
-    const [showConfirmation, setShowConfirmation] = useState(false)
+    const [showAlert, setShowAlert] = useState(false)
+    const [reservationError, setReservationError] = useState(null)
     const hour = props.hour
     const date = props.date
     const peopleValue = props.peopleValue
@@ -39,7 +40,8 @@ const ReservationSummary = (props) => {
           })
           .then(data => {
             console.log(data)
-            setShowConfirmation(true)
+            if (data.error === false) setReservationError(false)
+            else setReservationError(true)
           })
 
     }
@@ -48,11 +50,9 @@ const ReservationSummary = (props) => {
         <>
         {
         store.username && store.email ?
+          <>
+          {reservationError === null ? 
           <div className="reservation-summary">
-              {/* <div style={{display: 'flex', justifyContent: 'center', marginTop: '2%'}}>
-                  <h3 style={{fontWeight: 300}}>SZCZEGÓŁY REZERWACJI</h3>
-              </div> */}
-            
               <div className='reservation-summary-params'>
               <div className='summary-param'>
                     <FontAwesomeIcon className='param-icon' icon={faHouse}/><b><p className='param-value'>{props.restaurantName}</p></b>
@@ -72,7 +72,40 @@ const ReservationSummary = (props) => {
                   <button id='confirm-reservation' onClick={submitReservation}>POTWIERDŹ</button>
               </div>
           </div>
-            :
+          :
+          <>
+          {reservationError ?
+          <div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <FontAwesomeIcon className='param-icon' icon={faXmark}
+            style={{color: 'red', height: '80px', marginTop: '5%'}}/>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <h3>Ups, coś poszło nie tak</h3>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <p style={{marginBottom: '10%', textAlign: 'center'}}>Ta rezerwacja prawdopodobnie nie jest już dostępna. Prosimy o odświeżenie strony i ponowną próbę dokonania rezerwacji</p>
+          </div>
+        </div>
+          :
+          <div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <FontAwesomeIcon className='param-icon' icon={faCheck}
+              style={{color: 'green', height: '80px', marginTop: '5%'}}/>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <h3>Dziękujemy za dokonanie rezerwacji</h3>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <p style={{marginBottom: '10%', textAlign: 'center'}}>Szczegóły rezerwacji możesz przejrzeć w każdej chwili w panelu uzytkownika</p>
+            </div>
+            
+          </div>
+          }
+          </>
+          }
+          </>
+          :
           <div style={{display: 'flex', justifyContent: 'center'}}>
             <h3 style={{marginTop: '10%', marginBottom: '10%'}}>Zaloguj się, aby dokonać rezerwacji</h3>
           </div>
